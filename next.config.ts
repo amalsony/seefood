9c7e17d3015bb1171f15b594021dd17eac4f06ff
Amal Sony <amalsony5@gmail.com>
2026-05-22 14:32:49 -0400

    fix: brain not found error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. Tell Webpack NOT to touch the C++ ONNX engine
+  serverExternalPackages: ["onnxruntime-node"],
+
+  // 2. Force Vercel to copy your models folder into the serverless function
+  outputFileTracingIncludes: {
+    "/api/predict": ["./models/**/*"],
+  },
 };
 
 export default nextConfig;
